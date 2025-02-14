51a4939a10c1d84b03078fde47bf66ee8553fafc
Matheus Marciano <mm.leitea@outlook.com>
2025-02-14 15:19:59 +0000

    🚑️ Add navegation to user list page

---
+++ src/components/AppDrawer.vue
@@ -0,0 +1,16 @@
+<template>
+	<v-navigation-drawer
+	  v-model="drawer"
+	  temporary
+	>
+
+	<v-list-item title="Iguana Frontend Template" subtitle="Using Vuetify"></v-list-item>
+  	<v-divider></v-divider>
+	<v-list-item to="/user">Users</v-list-item>
+	</v-navigation-drawer>
+  </template>
+  
+  <script lang="ts" setup>
+  const drawer = ref(false)
+  </script>
+  
