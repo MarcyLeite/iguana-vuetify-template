8642682b9f8ce7c35b4eb0d25509814331dafd8e
Matheus Marciano <mm.leitea@outlook.com>
2025-02-17 23:35:40 +0000

    ♻️ Create AppDataList component

--- src/components.d.ts
+++ src/components.d.ts
@@ -7,6 +7,7 @@ export {}
 /* prettier-ignore */
 declare module 'vue' {
   export interface GlobalComponents {
+    AppDataList: typeof import('./components/AppDataList.vue')['default']
     AppDrawer: typeof import('./components/AppDrawer.vue')['default']
     AppFooter: typeof import('./components/AppFooter.vue')['default']
     ConfirmPrompt: typeof import('./components/ConfirmPrompt.vue')['default']
